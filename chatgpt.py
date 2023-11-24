05b5b88ef788b1d70561b245d66034e4f1032ba2
Federico Caimi <fedeoasi@gmail.com>
2023-11-24 14:24:41 -0600

    Minor changes to the script and upload own resume

--- chatgpt.py
+++ chatgpt.py
@@ -16,27 +16,34 @@ import constants
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
 # Enable to save to disk & reuse the model (for repeated queries on the same data)
-PERSIST = False
+PERSIST = True
 
 query = None
 if len(sys.argv) > 1:
   query = sys.argv[1]
 
-if PERSIST and os.path.exists("persist"):
-  print("Reusing index...\n")
-  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
-  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+persist_path = 'persist'
+
+if not PERSIST and not os.path.exists(persist_path):
+  print(f"Persist is set to True, but the desired path could not be found {persist_path}\n")
+
+if PERSIST and os.path.exists(persist_path):
+  if os.path.exists(persist_path):
+    print(f"Reusing index on path {persist_path}\n")
+    vectorstore = Chroma(persist_directory=persist_path, embedding_function=OpenAIEmbeddings())
+    index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+    
 else:
-  #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
   loader = DirectoryLoader("data/")
   if PERSIST:
-    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":persist_path}).from_loaders([loader])
   else:
     index = VectorstoreIndexCreator().from_loaders([loader])
 
 chain = ConversationalRetrievalChain.from_llm(
   llm=ChatOpenAI(model="gpt-3.5-turbo"),
   retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+  verbose=True
 )
 
 chat_history = []
