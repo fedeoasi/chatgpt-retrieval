2165177b4cf644e3105e7153b9bff507c0d8b5fd
Federico Caimi <fedeoasi@gmail.com>
2023-11-26 10:51:01 -0600

    More logging

--- chatgpt_retrieval_app/chatgpt_retrieval_app/__init__.py
+++ chatgpt_retrieval_app/chatgpt_retrieval_app/__init__.py
@@ -15,9 +15,13 @@ def init():
     os.environ["OPENAI_API_KEY"] = settings.OPENAI_API_KEY
     persist_path = settings.PERSIST_PATH
     
+    print(f"Opening vectorstore from path {persist_path}")
+
     vectorstore = Chroma(persist_directory=persist_path, embedding_function=OpenAIEmbeddings())
     index = VectorStoreIndexWrapper(vectorstore=vectorstore)
-            
+
+    print(f"Setting up retrieval chain")
+
     chain = ConversationalRetrievalChain.from_llm(
         llm=ChatOpenAI(model="gpt-3.5-turbo"),
         retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
@@ -26,6 +30,4 @@ def init():
     return chain
 
 print(sys.argv)
-
-chain = init()
 
